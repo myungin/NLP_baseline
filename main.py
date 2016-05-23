d5944cc27eef820bd3e2f8b4532c9a2d834e2307
Myungin Kim <myungin@galton.uchicago.edu>
2016-05-22 22:37:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+
+import Seq2SeqModel 
+import tokenizer
+#implementing seq2seq
+
+def main():
+    
+    #not completed yet
+    
+    #suppose we re given data with buckets
+    
+    
+    
+    mod=Model(source_vocab_size, target_vocab_size, buckets, size, num_layers)
+    
+    
+    batch1=mod.get_batch(data, 3)
+    
+    
+    
+    
