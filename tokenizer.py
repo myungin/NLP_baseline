b0e808b54c4618fb22e0e99231aa7bdd6fea6c0d
Myungin Kim <myungin@galton.uchicago.edu>
2016-05-22 20:36:03 -0500

    Add files via upload

---
+++ tokenizer.py
@@ -0,0 +1,45 @@
+##this codes was copied from "https://github.com/dennybritz/rnn-tutorial-rnnlm/blob/master/train-theano.py"
+
+def tokenizer(file1):
+    
+    # Read the data and append SENTENCE_START and SENTENCE_END tokens
+    print "Reading CSV file..."
+    with open(file1, 'rb') as f:
+        reader = csv.reader(f, skipinitialspace=True)
+        reader.next()
+        # Split full comments into sentences
+        sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+        sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for     x in sentences]
+    print "Parsed %d sentences." % (len(sentences))
+    
+    # Tokenize the sentences into words
+    tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+    # Count the word frequencies
+    word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+    print "Found %d unique words tokens." % len(word_freq.items())
+
+    # Get the most common words and build index_to_word and word_to_index vectors
+    vocab = word_freq.most_common(vocabulary_size-1)
+    index_to_word = [x[0] for x in vocab]
+    index_to_word.append(unknown_token)
+    word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+    print "Using vocabulary size %d." % vocabulary_size
+    print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+
+    # Replace all words not in our vocabulary with the unknown token
+    for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+    # Create the training data
+    X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+    y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+    return tokenized_sentences, X_train, y_train
+
+
+
+    
+    
